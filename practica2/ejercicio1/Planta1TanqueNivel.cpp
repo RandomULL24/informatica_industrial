f48e746ded7a8983c8e914031f931db22ca3faf8
RandomULL24 <alu0101430225@ull.edu.es>
2024-11-03 18:31:53 +0000

    Casi terminado. Pruebas 63,64 -> 98% y 66,67 -> 99%

--- practica2/ejercicio1/Planta1TanqueNivel.cpp
+++ practica2/ejercicio1/Planta1TanqueNivel.cpp
@@ -1,32 +1,54 @@
 //Autor: Lucas Díaz Pérez
 //Fecha: 3/11/2024
+#include "Planta1TanqueNivel.hpp"
 #include "Planta.hpp"
 #include "SensorNivel.hpp"
 #include "Valvula.hpp"
 #include <cmath>
+#include <string>
 
-void setNombre(std::string nombre)
+//Instanciamos los valores de la función.
+Planta1TanqueNivel::Planta1TanqueNivel(float caudalMaximo, float nivelMaximo) : 
+    senNivel(nivelMaximo), actValvula(caudalMaximo)
 {
+}
+
+void Planta1TanqueNivel::setNombre(std::string nombre)
+{
+ this->Planta::setNombre(nombre); 
+ (this->senNivel).setNombre(nombre + " SenNivel");
+ (this->actValvula).setNombre(nombre + " Valvula");
  
 }
 
-SensorNivel* getSensorNivelPt()
+SensorNivel* Planta1TanqueNivel::getSensorNivelPt()
 {
+  return &(this->senNivel);
 }
 
-Valvula* getValvulaPt()
+Valvula* Planta1TanqueNivel::getValvulaPt()
 {
+  return &(this->actValvula);
 }
 
-void enciende()
+void Planta1TanqueNivel::enciende()
 {
+  (this->senNivel).enciende();
+  (this->actValvula).enciende();
+  this->Planta::enciende();
+  
 }
 
-float getAltura
+float Planta1TanqueNivel::getAltura()
 {
+  return this->hk;
 }
 
-void calculaEtapa()
+void Planta1TanqueNivel::calculaEtapa()
 {
+  hk = (this->hk) + ((this->T)/(this->A))*((this->actValvula.getCaudal())
+      - (this->Z)*sqrt(this->hk));
+      
+  (this->hk) = hk;
 }
 
--- practica2/ejercicio1/Planta1TanqueNivel.hpp
+++ practica2/ejercicio1/Planta1TanqueNivel.hpp
@@ -4,6 +4,7 @@
 #include "SensorNivel.hpp"
 #include "Valvula.hpp"
 #include <cmath>
+#include <string>
 
 #ifndef _PLANTA1TANQUENIVEL_
 #define _PLANTA1TANQUENIVEL_
@@ -11,20 +12,23 @@
 class Planta1TanqueNivel : public Planta
 {
   private:
-    SensorNivel senNivel(float alturaMaxima);
-    Valvula actValvula(float caudalMax);
+    SensorNivel senNivel;
+    Valvula actValvula;
+    std::string nombre;
     
-    float hk = NAN;
-    float A = 2.0;
-    float Z = 0.1;
-    float T = 0.5;
+    float hk = 0.0;
+    float A  = 2.0;
+    float Z  = 0.1;
+    float T  = 0.5;
   
   public:
+    Planta1TanqueNivel(float caudalMaximo, float nivelMaximo);
     void setNombre(std::string nombre);
+    
     SensorNivel* getSensorNivelPt();
     Valvula* getValvulaPt();
     void enciende();
-    float getAltura;
+    float getAltura();
     void calculaEtapa();
 };
 
