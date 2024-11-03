841e1d13d4b94a7386bdd0c0c74f115632480c3c
RandomULL24 <alu0101430225@ull.edu.es>
2024-11-03 13:26:09 +0000

    Se ha creado la clase hija ControladorONOFF de Controlador y ya con eso hemos completado las pruebas 35,36,37 -> 100% y 38 -> 50% y 39 -> 55%

---
+++ practica2/ejercicio1/ControladorONOFF.cpp
@@ -0,0 +1,7 @@
+//Autor: Lucas Díaz Pérez
+//Fecha: 3/11/2024
+#include <cmath>
+#include "ControladorONOFF.hpp"
+#include "Controlador.hpp"
+#include "Dispositivo.hpp"
+#include <cmath>
---
+++ practica2/ejercicio1/ControladorONOFF.hpp
@@ -0,0 +1,16 @@
+//Autor: Lucas Díaz Pérez
+//Fecha: 3/11/2024
+#include <cmath>
+#ifndef _CONTROLADORONOFF_
+#define _CONTROLADORONOFF_
+#include "Dinamico.hpp"
+#include "Actuador.hpp"
+#include "Sensor.hpp"
+#include "Controlador.hpp"
+
+class ControladorONOFF : public Controlador
+{
+
+};
+
+#endif
