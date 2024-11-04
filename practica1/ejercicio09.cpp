c203b7efdef78a2d18b6bc0934951313e2136d6a
RandomULL24 <alu0101430225@ull.edu.es>
2024-11-04 12:47:41 +0000

    Ejercicio9 formateado

--- practica1/ejercicio09.cpp
+++ practica1/ejercicio09.cpp
@@ -5,48 +5,46 @@
 
 std::string numeroBits(unsigned valor)
 {
-	int totalBits = sizeof(valor)*8;
-	std::string versionString;
-	bool unoVisto = false;
+  int totalBits = sizeof(valor)*8;
+  std::string versionString;
+  bool unoVisto = false;
 
-	for(int i = (totalBits - 1); i >= 0; i--){
-		
-		int mascara = 1 << i;
-		
-		if(valor & mascara)
-		{
-			versionString =  versionString + '1';
-			unoVisto = true;
-		}
-		else
-		{
-			if(unoVisto)
-			{
-				versionString = versionString + '0';
-			}
-		}
-	}
+  for(int i = (totalBits - 1); i >= 0; i--){
+
+  int mascara = 1 << i;
+
+    if(valor & mascara)
+    {
+      versionString =  versionString + '1';
+      unoVisto = true;
+    }
+    else
+    {
+      if(unoVisto)
+      {
+        versionString = versionString + '0';
+      }
+    }
+  }
   
   if(unoVisto == false)
   {
     versionString = '0';
   }
-	
-	
-	
+
 	return versionString;
 }
 
 int main()
 {
-	unsigned entero;
+  unsigned entero;
   //manejamos la entrada como string para poder detectar el fallo de 
   //conversión a unsgined en caso de que no se trate de uno (ej: xx)
   std::string input;
-	
-	do
-	{
-		std::cin >> input;
+
+  do
+  {
+    std::cin >> input;
     try
     {
       entero = std::stoul(input); //Si NO es unsigned manda una execpción
@@ -56,12 +54,10 @@ int main()
         return 1;
     }
     
-		
-		std::cout << numeroBits(entero) << " 0x" << std::hex << entero  
-				<< std::endl;
-	
-	}
-	while(entero != 0);
-	
-	return 0;
+    std::cout << numeroBits(entero) << " 0x" << std::hex << entero  
+        << std::endl;
+  }
+  while(entero != 0);
+
+  return 0;
 }
